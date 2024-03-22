2b2fd6543ab3b73e676d047fa532b438b772ea6e
ehsansdr <sehsan251@gmail.com>
2024-03-22 15:26:18 +0330

    game panel done

--- src/Const/ConstData.java
+++ src/Const/ConstData.java
@@ -7,13 +7,14 @@ public class ConstData {
          public static final int FRAME_WIDTH = 450;
          public static final int FRAME_HEIGHT = 600;
 
-         public static final int GamePanel_WIDTH = FRAME_WIDTH;
-         public static final int GamePanel_HEIGHT = FRAME_WIDTH;
+         public static final int GamePanel_WIDTH = FRAME_WIDTH - 16;
+         public static final int GamePanel_HEIGHT = FRAME_WIDTH - 14;
 
-         public static final int upper_gap = FRAME_HEIGHT - GamePanel_WIDTH;
+         public static final int upper_gap = 125;
          public static final Color frameBackground = new Color(0x000000);
          public static final Color bigPanelBackground = new Color(0x2A343D);
          public static final Color gamePanelBackground = new Color(0x14213D);
+         public static final Color gamePanelLinesBackground = new Color(0xE5E5E5);
 
      }
  //UIManager.put("activeCaption", new javax.swing.plaf.ColorUIResource(
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -18,6 +18,22 @@ public class GamePanel extends JPanel implements MouseListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        g.setColor(gamePanelLinesBackground);
+
+        //vertical lines
+        g.drawLine(0,0,0,GamePanel_HEIGHT);//first vertical line
+        for (int i = 1; i <= 2;i++){
+            g.drawLine(i * (GamePanel_WIDTH / 3 - 1),0,i * (GamePanel_WIDTH / 3 - 1),GamePanel_HEIGHT);
+        }
+        g.drawLine(GamePanel_WIDTH - 1,0,GamePanel_WIDTH - 1,GamePanel_HEIGHT);//last vertical line
+
+
+        //horizontal lines
+        g.drawLine(0,0,GamePanel_WIDTH,0);//first horizontal line
+        for (int i = 1; i <= 2;i++){
+            g.drawLine(0,i * (GamePanel_WIDTH / 3 - 1),GamePanel_WIDTH,i * (GamePanel_WIDTH / 3 - 1));
+        }
+        g.drawLine(0,GamePanel_HEIGHT - 1,GamePanel_WIDTH - 1,GamePanel_HEIGHT - 1);//last horizontal line
     }
 
     @Override
