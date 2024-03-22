fb922570db0253ccb56807f21879f3c57dd6dcf9
ehsansdr <sehsan251@gmail.com>
2024-03-22 16:06:17 +0330

    game lope added

--- src/TicGame.java
+++ src/TicGame.java
@@ -2,17 +2,75 @@ import javax.swing.*;
 import java.awt.*;
 import static Const.ConstData.Data.*;
 
-public class TicGame {
+public class TicGame implements Runnable {
     JFrame frame = new JFrame();
     JPanel bigPanel = new JPanel();
     GamePanel gamePanel = new GamePanel();
 
+    private int fps = 120;
+    private int UPS = 120;
+    Thread gameLoopThread;
 
 
     public TicGame() {
         frameCreation();
         bigPanelCreation();
         addingComponents();
+
+
+        gameLoopThread = new Thread(this);
+        gameLoopThread.start();
+
+    }
+    @Override
+    public void run(){
+        double timePerFrame = 1000000000.0 / fps;
+        double timePerUpdate = 1000000000.0 / UPS;
+
+        long previousTime = System.nanoTime();
+
+        int frames = 0;
+        int updates = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        double deltaU = 0;
+        double deltaF = 0;
+
+        while (true) {
+            long currentTime = System.nanoTime();
+
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
+
+            if (deltaU >= 1) {
+                //put your statement in this part
+
+                //
+
+                /** DO NOT OMIT THESE TWO STATEMENT*/
+                updates++;
+                deltaU--;
+            }
+
+            if (deltaF >= 1) {//for Fps
+                //put your statement in this part
+
+
+                //
+
+                /** DO NOT OMIT THESE TWO STATEMENT*/
+                frames++;
+                deltaF--;
+            }
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {//every 1 sec
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames +" | " + updates );
+                frames = 0;
+                updates = 0;
+            }
+        }
     }
 
 
@@ -22,7 +80,6 @@ public class TicGame {
         bigPanel.setLayout(null);
         bigPanel.setBackground(bigPanelBackground);
     }
-
     private void frameCreation() {
         frame.setTitle("Tic Tac Toe");
         frame.setSize(new Dimension(FRAME_WIDTH,FRAME_HEIGHT));
